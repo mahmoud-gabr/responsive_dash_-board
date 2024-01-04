097c65e7239c03541d5dc41dc98ff06a14d09078
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2024-01-04 13:08:27 +0200

    generate addets

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,51 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.svg
+  static const String imagesAvatar = "assets/images/avatar.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCard
+  /// assets/images/card.svg
+  static const String imagesCard = "assets/images/card.svg";
+
+  /// Assets for imagesDashboard
+  /// assets/images/dashboard.svg
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesInvestments
+  /// assets/images/investments.svg
+  static const String imagesInvestments = "assets/images/investments.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesSetting2
+  /// assets/images/setting-2.svg
+  static const String imagesSetting2 = "assets/images/setting-2.svg";
+
+  /// Assets for imagesStatics
+  /// assets/images/statics.svg
+  static const String imagesStatics = "assets/images/statics.svg";
+
+  /// Assets for imagesTransaction
+  /// assets/images/transaction.svg
+  static const String imagesTransaction = "assets/images/transaction.svg";
+
+  /// Assets for imagesWallet2
+  /// assets/images/wallet-2.svg
+  static const String imagesWallet2 = "assets/images/wallet-2.svg";
+}
