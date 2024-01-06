c9558b60601071445d88fe98931ba33fac3cd263
Mahmoud Gabr <mahmoudgabr1121@gmail.com>
2024-01-06 15:55:48 +0200

    create active & in active drawer item

--- lib/views/widgets/drawer_item.dart
+++ lib/views/widgets/drawer_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_style.dart';
+import 'package:responsive_dash_board/views/widgets/active%20&%20in_Active_drawer_item.dart';
 
 class DrawerItem extends StatelessWidget {
   const DrawerItem(
@@ -13,44 +14,5 @@ class DrawerItem extends StatelessWidget {
     return isActive
         ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
         : InActiveDrawerItem(drawerItemModel: drawerItemModel);
-  }
-}
-
-class InActiveDrawerItem extends StatelessWidget {
-  const InActiveDrawerItem({
-    super.key,
-    required this.drawerItemModel,
-  });
-  final DrawerItemModel drawerItemModel;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(drawerItemModel.icon),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyle.styleRegulare16,
-      ),
-    );
-  }
-}
-
-class ActiveDrawerItem extends StatelessWidget {
-  const ActiveDrawerItem({super.key, required this.drawerItemModel});
-  final DrawerItemModel drawerItemModel;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(drawerItemModel.icon),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyle.styleBold16,
-      ),
-      trailing: Container(
-        width: 3.27,
-        decoration: const BoxDecoration(
-          color: Color(0xFF4EB7F2),
-        ),
-      ),
-    );
   }
 }
